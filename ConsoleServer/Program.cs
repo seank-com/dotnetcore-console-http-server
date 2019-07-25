1404ef679a6ddfb62d8f2c2638cbd6e289af13de
Sean Kelly <home@seank.com>
2019-07-25 08:08:03 -0700

    Initial Commit

---
+++ ConsoleServer/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace ConsoleServer
+{
+    using Microsoft.AspNetCore.Builder;
+    using Microsoft.AspNetCore.Hosting;
+    using Microsoft.AspNetCore.Http;
+    using System;
+    using System.IO;
+
+    public class Program
+    {
+        static public void Configure(IApplicationBuilder app)
+        {
+            app.Run(async (context) => {
+                Console.WriteLine(context.Request.Path);
+
+                if (context.Request.ContentType != null)
+                {
+                    Console.WriteLine("Content-Type: {0}", context.Request.ContentType);
+                }
+                Console.WriteLine("Content-Length: {0}", context.Request.ContentLength);
+
+                string inputBody;
+                using (var reader = new StreamReader(context.Request.Body, System.Text.Encoding.UTF8))
+                {
+                    inputBody = await reader.ReadToEndAsync();
+                }
+
+                Console.WriteLine("-----START-BODY-CONTENT-----");
+                Console.WriteLine(inputBody);
+                Console.WriteLine("-----END-BODY-CONTENT-----");
+
+                context.Response.StatusCode = 200;
+                await context.Response.WriteAsync("OK");
+            });
+        }
+        static void Main()
+        {
+            new WebHostBuilder().UseKestrel().Configure(Configure).UseUrls("http://+:80").Build().Run();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # dotnetcore-console-http-server
  A simple sample of a .Net Core console application that receives json from a client
+
+## Build
+Load solution in Visual Studio 2019 or later and press <kbd>F5</kbd>
+
+## Purpose
+This was a test app to prove that the code in the IoTEdge SensorModuleHub would work.
